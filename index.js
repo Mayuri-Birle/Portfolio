d4c7206bc529b0b5f2a44dc754e3b915c9f62936
Mayuri Birle <mayuri.birle09@gmail.com>
2020-05-17 08:10:22 +0530

    initial

---
+++ index.js
@@ -0,0 +1,31 @@
+
+$(window).scroll(function (e) {
+
+    // add/remove class to navbar when scrolling to hide/show
+    var scroll = $(window).scrollTop();
+    if (scroll >= 150) {
+        $('.navbar').addClass("navbar-hide");
+    } else {
+        $('.navbar').removeClass("navbar-hide");
+    }
+
+});
+var typed = new Typed("#typed", {
+    strings: [
+        "Web Developer",
+        "Learner",
+        "enthusiastic and little bit sassy",
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+});
+var typed_2 = new Typed("#typed_2", {
+    strings: [
+        "Web Developer",
+
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+});
